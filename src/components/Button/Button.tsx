5450fcc61b645ba594e00cf630ef9b55d56facff
mukwende <mukwende2000@gmail.com>
2023-06-13 13:08:35 +0200

    write part of the checkout page

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,16 @@
-import { ReactNode } from "react";
-
 type Props = {
   backgroundColor: string;
   color: string;
+  text: string
 };
-function Button({ backgroundColor, color }: Props) {
+function Button({ backgroundColor, color, text='see product' }: Props) {
   return (
     <button
       className={`${backgroundColor} ${color} py-3 px-7 text-[.9rem] uppercase duration-200 ${
         backgroundColor.includes("bg-transparent") ? "border" : null
       }`}
     >
-      see product
+     { text }
     </button>
   );
 }
